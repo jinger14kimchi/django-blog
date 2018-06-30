f35f6816d705ecd4f5344702f04d637ee4c32996
Jennifer Ricardo <set>
2018-06-30 16:33:55 +0800

    Added new app

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.news_list, name='news_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.news_detail, name='news_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import News
+# from .forms import PostForm
+from django.shortcuts import redirect
 
 def news_list(request):
-    return render(request, 'news/news_list.html', {})
-  
+    news = News.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'news/news_list.html', {'news': news})
+
+
+def news_detail(request, pk):
+    news = get_object_or_404(News, pk=pk)
+    return render(request, 'news/news_detail.html', {'news': news})
